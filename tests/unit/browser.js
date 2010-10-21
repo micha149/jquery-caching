8d740e27451e7d14f9688c1dd090c931d8e58042
Michael van Engelshoven <michael@van-engelshoven.de>
2010-10-21 08:20:22 +0200

    Added tests for JSON object

--- tests/unit/browser.js
+++ tests/unit/browser.js
@@ -2,9 +2,22 @@ jQuery(function() {
 
    module('browser');
 
+   /**
+    * Tests the browsers local storage functionalities
+    */
    test('LocalStorage', function() {
 
        ok(typeof window.localStorage == 'object', 'Local Storage exists');
+
+   });
+
+   /**
+    * Tests the JSON helper object
+    */
+   test('JSON', function() {
+
+       ok(typeof JSON == 'object', 'JSON object exists');
+       ok(typeof JSON.stringify == 'function', 'JSON.stringify exists')
 
    });
    
